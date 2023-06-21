37026332bb159ed86aac70c66820baf284b49d95
pica69co <oscarwarrieta@gmail.com>
2023-06-21 16:12:41 -0500

    feature/modalDetail-rating-description

--- app/product/[id]/not-found.tsx
+++ app/product/[id]/not-found.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
     <div className="mt-24">
         <h2>Not Found</h2>
         <p>Could not find requested resource</p>
-        <p>View <Link href="/">All Products</Link></p>
+        <p>View <Link href="/">Home</Link></p>
     </div>
   )
 }
